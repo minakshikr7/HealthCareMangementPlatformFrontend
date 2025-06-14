92cf754f53d0d54262fbc1cff05b400c9bf711ae
minakshikr7 <minakshi052005@gmail.com>
2025-06-14 11:04:31 +0530

    fix bugs

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// /pages/index.jsx
+import Home from '@/Components/Home';
+import React from 'react';
+ // if Home is a component you made in pages
+
+export default function Index() {
+  return <Home/>;
+}
